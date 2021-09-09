0c2064989d2123398326b80b2c23ddfd038bf3bc
NatalyMora21 <tatamora21gmail.com>
2021-09-08 23:43:05 -0500

    Created attribute to stop or continue game

--- models/Board.js
+++ models/Board.js
@@ -8,12 +8,13 @@ export default class Board {
         this.gameOver=false;
         this.bars=[];
         this.ball= null;
+        this.playing= false;
 
     }
 
     get elements () {
         //bars -> Barras lateras que controlan el juego
-        let elementos= this.bars;
+        let elementos= this.bars.map(bar =>bar);
         elementos.push(this.ball);
         return elementos;
 
