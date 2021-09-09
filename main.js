18bb9091278ca547881b4023660761d70240dda0
NatalyMora21 <tatamora21gmail.com>
2021-09-08 23:44:31 -0500

    Created validations of movements according to the keys

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Board from './models/Board.js'
 import BoardView from './models/BoardView.js'
 import Bar from './models/Bar.js'
+import Ball from './models/Ball.js';
 
 
 
@@ -9,42 +10,56 @@ var bar1 = new Bar(45,100,10,100, board);
 var bar2 = new Bar(735,100,10,100, board);
 let canvas= document.getElementById('canvas');
 let boardViewn= new BoardView(canvas,board);
+let ball = new Ball(350,100,10, board);
 
 
 
 document.addEventListener("keydown",(ev)=>{
-    ev.preventDefault();
+
     //ev-> Trae info del evento
     console.log(ev.keyCode);
-    if(ev.keyCode==38){
-        bar1.up();
 
-    }
-    else if (ev.keyCode==40){
-        bar1.down()
+
+    switch (ev.keyCode) {
+        case 38 : 
+            ev.preventDefault();
+            bar1.up();
+            break;
+        case 40 : 
+            ev.preventDefault();
+            bar1.down();
+            break;
+        case 87 : 
+            ev.preventDefault();
+            //S
+            bar2.up();
+            break;
+        case 83 : 
+            ev.preventDefault();
+            //w
+            bar2.down();
+            break;
+        case 32 :
+            ev.preventDefault();
+            board.playing = !board.playing;
     }
 
-    else if(ev.keyCode==87){
-        bar2.up();
-
-    }
-    else if (ev.keyCode==83){
-        bar2.down()
-    }
 
     console.log(bar1.toString())
 })
 
+boardViewn.draw();
 window.addEventListener("load",controller)
 window.requestAnimationFrame(controller)
+
+setTimeout(()=>{ball.direction=-1},4000)
 
 function controller () {
     //console.log("prueb")
     //
 
-    boardViewn.clean()
-    boardViewn.draw()
-    window.requestAnimationFrame(controller)
+    boardViewn.play();
+    window.requestAnimationFrame(controller);
     
 
 }
