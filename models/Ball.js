1d17d18b0b5effc40e933b03b1485a491ab31612
NatalyMora21 <tatamora21gmail.com>
2021-09-08 23:42:10 -0500

    Created function to move the ball from one side to the other

--- models/Ball.js
+++ models/Ball.js
@@ -7,9 +7,15 @@ export default class Ball{
         this.speedy = 0;
         this.speedx = 3;
         this.board = board;
+        this.direction = 1;
 
         board.ball = this;
         this.kind = "circle"
 
+    }
+
+    move () {
+        this.x += (this.speedx * this.direction);
+        this.y += (this.speedy )
     }
 }
