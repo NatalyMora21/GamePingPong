a49ee0eb8ee868582af954150111cef01e560f00
NatalyMora21 <tatamora21gmail.com>
2021-09-08 21:39:09 -0500

    Two new bars instantiated

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 import Board from './models/Board.js'
 import BoardView from './models/BoardView.js'
+import Bar from './models/Bar.js'
 
 function main () {
 
     let board= new Board(800,400);
+    let bar1 = new Bar(20,100,10,100, board);
+    let bar2 = new Bar(700,100,10,100, board);
     let canvas= document.getElementById('canvas');
     let boardViewn= new BoardView(canvas,board);
     boardViewn.draw()
 
 
-    console.log(board)
+
 
 }
 
