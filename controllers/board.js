20edeafb58eb3afd26b42452b5b1462d88d5f12a
NatalyMora21 <tatamora21gmail.com>
2021-09-08 21:38:04 -0500

    Added validation on empty element

--- controllers/board.js
+++ controllers/board.js
@@ -1,14 +1,15 @@
 
 
-export function draw(ctx,element){
+export default function drawn(ctx,element){
 
-    if(element!= null ){
+    if(element!== null && element.hasOwnProperty("kind") ){
         switch(element.kind) {
             case "square": 
                 ctx.fillRect(element.x,element.y,element.width,element.height);
                 break;
             case "rectangulo": 
                 ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
         }
     }
 
